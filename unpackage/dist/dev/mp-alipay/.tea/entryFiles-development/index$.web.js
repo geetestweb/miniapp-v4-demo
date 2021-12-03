4493c5f76a9a0afb1e051face1bd1216e9cde1b5
lihuawu <lihuawu@geetest.com>
2021-12-03 15:43:28 +0800

    init

--- unpackage/dist/dev/mp-alipay/.tea/entryFiles-development/index$.web.js
+++
@@ -1,5 +0,0 @@
-/**! __CODEPLACEHOLDER_START__ */ /*[PositionForClientEntryCodeBegin]*/ /**! __CODEPLACEHOLDER_END__ */
-require('@alipay/appx-compiler/lib/sjsEnvInit');
-require('./config$');
-require('../../app');
-require('../../pages/index/index?hash=0a99177ac889ba4184de6acbc638b5778d80f936');
